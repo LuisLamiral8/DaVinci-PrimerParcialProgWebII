0c58430ff9123342f7a6edd51b5a894a10cc556f
Luis <67067808+LuisLamiral8@users.noreply.github.com>
2025-04-28 15:36:25 -0300

    First commit

---
+++ index.php
@@ -0,0 +1,99 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title> Supermercado </title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <div class="container">
+
+        <h1 class="mb-3"> Detalle de compra </h1>
+
+        <div class="border-bottom-3">
+            <h2> Cofre de descuentos </h2>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th> Moneda </th>
+                        <th> Cantidad </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <img src="img/iconos/detalle/oro.png" alt="Oro" />
+                            Oro ($200)
+                        </td>
+                        <td> </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <img src="img/iconos/detalle/plata.png" alt="Oro" />
+                            Plata ($50)
+                        </td>
+                        <td> </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <img src="img/iconos/detalle/bronce.png" alt="Oro" />
+                            Bronce ($10)
+                        </td>
+                        <td> </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <img src="img/iconos/detalle/bonif.png" alt="Bonificación" />
+                            Bonificación
+                        </td>
+                        <td> </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <div class="border-bottom-3">
+            <h2> Productos </h2>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th> Cod </th>
+                        <th> Detalle </th>
+                        <th> Precio </th>
+                        <th> Cantidad </th>
+                        <th> Subtotal </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <img src="img/iconos/categorias/bebidas.png" alt="">
+
+                            111
+                        </td>
+                        <td> Coca Cola 1.5LT </td>
+                        <td> $100,00 </td>
+                        <td> 3 </td>
+                        <td> $300,00 </td>
+                    </tr>
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td colspan="5">
+                            Total:
+                        </td>
+                    </tr>
+                </tfoot>
+            </table>
+        </div>
+
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+</body>
+
+</html>
